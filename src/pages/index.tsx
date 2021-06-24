805c03117a545c8b76d6b1a6a8eb8c249e2dd9aa
Hardik Sachan <sachanhardik@gmail.com>
2021-06-24 07:17:05 +0530

    ui: carousel done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
       </Head>
 
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+      </main>
     </div>
   );
 };
